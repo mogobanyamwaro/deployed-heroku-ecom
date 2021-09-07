0d2c0e5994b3c7f6a03b2ca4bc75ca6a5f11872d
mogobanyamwaro <mogobanyamwaro@gmail.com>
2021-09-07 08:45:06 +0300

    added all

---
+++ config/database.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config({ path: 'config/config.env' });
+
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config({ path: 'config/config.env' });
+}
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      connectTimeoutMS: 5000,
+      socketTimeoutMS: 5000,
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDatabase;
